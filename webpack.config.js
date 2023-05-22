9a70453e4d6642231ff3003dbcaeede19668e9f8
fernando.bendala <f.bendala@shopfully.com>
2023-05-22 11:21:27 +0200

    hot-fix: routing webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,6 @@ const config = ( env, argv ) => {
                             ],
                         },
                         noErrorOnMissing: true,
-                        to: 'img/[name][ext]',
                     },
                 ],
             } ),
